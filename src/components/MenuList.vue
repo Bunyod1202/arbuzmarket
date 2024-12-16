4c82f1c90d2406e4ca37f5d603f54cc9628dbcd8
Bunyood <bunyodbunyod035@gmail.com>
2024-12-16 16:10:37 +0500

    first commit

---
+++ src/components/MenuList.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="product-list">
+    {{ products }}
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      products: [],
+    }
+  },
+  methods: {
+    async fetchProducts() {
+      try {
+        const response = await axios.get('https://arbuzmarket.com/search') // Replace with your actual API endpoint
+        this.products = response.data
+      } catch (error) {
+        console.error('Error fetching products:', error)
+      }
+    },
+  },
+  mounted() {
+    this.fetchProducts()
+  },
+}
+</script>
+
+<style>
+.product-list {
+  gap: 24px 22px;
+  grid-template-columns: repeat(4, minmax(0px, 1fr));
+}
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="">
+    <a class="product-card" target="blank" :href="productLink">
+      <div class="product-card__details">
+        <img :src="imageSrc" :alt="productName" class="product-card__image" />
+        <div class="product-card__name">{{ productName }}</div>
+      </div>
+
+      <div class="product-card__price">
+        <span class="product-card__price__normal">{{ price }} AED</span>
+      </div>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProductCard',
+  props: {
+    imageSrc: String,
+    price: Number,
+    productName: String,
+    promosionalPrice: Number,
+    productLink: String,
+  },
+}
+</script>
+
+<style scoped>
+.product-card {
+  text-decoration: none;
+  height: 100%;
+  width: 170px;
+  border: 2px solid #ddd;
+  border-radius: 8px;
+  padding: 16px;
+  text-align: center;
+  background-color: #fff;
+  margin: auto;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.product-card__image {
+  max-width: 100%;
+  height: 150px;
+  margin-bottom: 10px;
+}
+
+.product-card__price {
+  position: relative;
+  font-size: 18px;
+  font-weight: bold;
+
+  /* background: #004d00; */
+  color: #000000;
+  padding: 4px 18px;
+  border-radius: 4px;
+  border: 4px solid #004d00;
+  gap: 11px;
+  grid-template-columns: 1fr 0.2fr;
+}
+.product-card__price__promosional {
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(0, -120%);
+  color: red;
+  text-decoration: line-through;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+}
+.product-card__name {
+  text-align: start;
+  font-weight: 600;
+  font-size: 13.8906px;
+  line-height: 20px;
+  color: #1a202c;
+}
+</style>
