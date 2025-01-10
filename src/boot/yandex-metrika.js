61177d5051361b4f3010d65e3cd4f5fbdbb31848
Bunyood <bunyodbunyod035@gmail.com>
2025-01-10 18:39:13 +0500

    q

--- src/boot/yandex-metrika.js
+++ src/boot/yandex-metrika.js
@@ -2,7 +2,7 @@ import VueYandexMetrika from 'vue-yandex-metrika'
 
 export default ({ app }) => {
   app.use(VueYandexMetrika, {
-    id: 12345678, // Replace with your Yandex Metrica ID
+    id: 99475151, // Replace with your Yandex Metrica ID
     router: app.router, // Pass the router instance for SPA tracking
     options: {
       accurateTrackBounce: true,
